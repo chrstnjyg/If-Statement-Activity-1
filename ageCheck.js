03f4ec6ec79ce9b62c35aa0a9627163e5857cf4d
Christine <guerrerochristinejoy15@gmail.com>
2024-10-08 21:33:20 +0800

    ageCheck.js and index.html

---
+++ ageCheck.js
@@ -0,0 +1,12 @@
+function askAge(){
+    let age = prompt("Enter your age:");
+    if (age < 13) {
+        alert("You are a child.");
+    } else if (age >= 13 && age <= 19) { 
+        alert("You are a teenager.");
+    } else if (age >= 20) {
+        alert("You are an adult.");
+    } else {
+        alert("Try again. Please enter your age.");
+    }
+}
